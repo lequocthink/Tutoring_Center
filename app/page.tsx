90ac3b2e17179629c20ff45130001bfe05b284ad
thinh <lqt542001@gmail.com>
2023-08-31 20:01:18 +0700

    build CourseCompnent and search page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 import SliderMain from "./(components)/SliderMain";
+import CourseComponent from "./(components)/CourseComponent";
+import getAllCourses from "./actions/getAllCourses";
 
 const images = [
   "/a.jpg",
   "b.jpg"
 ]
 
-export default function Home() {
+interface HomeProps {
+  searchParams:string
+}
+
+
+export default async function Home({searchParams}: HomeProps) {
+
+  const courses = await getAllCourses(searchParams);
+
   return (
     <main className="w-[100%]">
       <SliderMain
         images={images}
       />
+
+      <div>
+        <div className="flex flex-wrap px-8">
+          {courses.map((item:any) => (
+            <CourseComponent
+              key={item.id}
+              data={item} 
+              currentUser={null}
+            />
+          ))}
+        </div>
+      </div>
   </main> 
   )
 }
